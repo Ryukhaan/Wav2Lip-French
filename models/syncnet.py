e3fc33f761de1d82676b003f9ec0abb4ab7a54c1
Ryukhaan <remi.decelle@protonmail.com>
2024-04-24 15:46:22 +0200

    conv2d to conv_block

--- models/syncnet.py
+++ models/syncnet.py
@@ -79,7 +79,7 @@ class SyncNet_color_Lora(nn.Module):
         self.config = LoraConfig(
             r=r,
             lora_alpha=lora_alpha,
-            target_modules=["conv2d"],
+            target_modules=["conv_block"],
             lora_dropout=dropout,
             bias="none",
         )

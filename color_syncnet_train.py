46044479a8fa812109c36d71019b18c3a50f39bb
Ryukhaan <remi.decelle@protonmail.com>
2024-05-16 11:39:45 +0200

    New listing from data root directory

--- color_syncnet_train.py
+++ color_syncnet_train.py
@@ -15,7 +15,7 @@ import numpy as np
 from glob import glob
 
 import os, random, cv2, argparse
-from hparams import hparams, get_image_list
+from hparams import hparams, get_image_list, get_image_list_from_directory
 from sklearn.model_selection import train_test_split
 
 from torch.utils.tensorboard import SummaryWriter
@@ -281,7 +281,7 @@ if __name__ == "__main__":
     # train_dataset = Dataset('train')
     # test_dataset = Dataset('val')
 
-    filenames = get_image_list(args.data_root, 'train')
+    filenames = get_image_list_from_directory(args.data_root) #get_image_list(args.data_root, 'train')
     seed = 0
     train_list, val_list = train_test_split(np.array(filenames), random_state=seed, train_size=0.8, test_size=0.2)
     print(len(train_list), train_list[:5])
--- hparams.py
+++ hparams.py
@@ -12,6 +12,12 @@ def get_image_list(data_root, split):
 
 	return filelist
 
+def get_image_list_from_directory(data_root):
+	filelists = []
+	for directory in os.listdir(data_root):
+		for subdir in os.listdir(directory):
+			filelists.append(os.path.join(data_root, directory, subdir))
+
 class HParams:
 	def __init__(self, **kwargs):
 		self.data = {}
